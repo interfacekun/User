7fd7bc7e3da02a9c7538ee4d1c5e6b068d333f30
interfacekun <627795061@qq.com>
2017-01-18 11:30:48 +0800

    add time 2017年 01月 18日 11:30:38 星期三 CST

--- addcurrenttime.py
+++ addcurrenttime.py
@@ -1,15 +1,18 @@
 import sublime
 import sublime_plugin
 import os
-
+import datetime
 
 class AddCurrentTimeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		now_time = os.popen('date').readlines()
+		#now_time = os.popen('date')
+		week = {'0': '星期天', '1': "星期一", '2': '星期二', '3': '星期三', '4': '星期四', '5': '星期五', '6': '星期六'}
+		today = week[datetime.datetime.now().strftime("%w")]
+
 		self.view.run_command("insert_snippet",
 			{
 
-				"contents": now_time
+				"contents": datetime.datetime.now().strftime("%Y年 %m月 %d日 %H:%M:%S "+ today+" CST")
 
 			}
 		)
